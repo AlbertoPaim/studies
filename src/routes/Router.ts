4eb86bfd371eca3255c5611d8e311f60ae43bed7
Alberto Paim <albertoopaim@gmail.com>
2024-07-16 10:38:25 -0300

    prisma

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,8 +1,4 @@
 import express from "express";
-import { jsonPost, teste, user } from "../controllers/teste";
 
 export const router = express.Router();
 
-router.get('/teste', teste);
-router.post('/user', user);
-router.get('/posts', jsonPost);
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import Express from "express";
 import { router } from "./routes/Router";
+import "dotenv/config";
 
 const app = Express();
 
 app.use(Express.json())
 app.use(Express.urlencoded({ extended: true }))
 app.use(router)
-app.listen(3000);
+app.listen(process.env.PORT);
 
 app.get('/ping', (req, res) => {
     res.send("pong");
-    console.log("Rodando: http://localhost:3000")
+    console.log(`Rodando na porta ${process.env.PORT}`)
 })
