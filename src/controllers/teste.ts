5d5e8d9678f2fe8cc0a50fc52d4020857d249035
Alberto Paim <albertoopaim@gmail.com>
2024-07-15 21:14:57 -0300

    zod test

--- src/controllers/teste.ts
+++ src/controllers/teste.ts
@@ -1,14 +1,42 @@
 import { RequestHandler } from "express"
+import { z } from "zod"
+import { postSchema } from "../schemas/postSchema"
 
 export const teste: RequestHandler = (req, res) => {
     return res.json({ message: "hello" })
 }
 
 export const user: RequestHandler = (req, res) => {
-    const { name, age } = req.body;
+    const userSchema = z.object({
+        name: z.string().min(2),
+        age: z.number().min(18).max(99)
+    })
 
-    console.log(name, age);
+    const result = userSchema.safeParse(req.body);
+
+    if (!result.success) {
+        return res.json({ message: "algo deu errado" }
+        )
+    }
+
+    const { name, age } = result.data;
 
     return res.json({ result: name, age })
 
 }
+
+export const jsonPost: RequestHandler = async (req, res) => {
+
+    const request = await fetch('http://jsonplaceholder.typicode.com/posts');
+    const data = await request.json();
+
+    const result = postSchema.safeParse(data);
+
+    if (!result.success) {
+        return res.status(500).json({ message: result.success })
+    }
+
+    let totalPosts = result.data.length;
+
+    res.json({ total: totalPosts })
+};
--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { teste, user } from "../controllers/teste";
+import { jsonPost, teste, user } from "../controllers/teste";
 
 export const router = express.Router();
 
 router.get('/teste', teste);
 router.post('/user', user);
+router.get('/posts', jsonPost);
